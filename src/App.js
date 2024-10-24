349e1f883e1059bae496ca5b88fa1325e1da71a6
avneet2005 <avneet@madvit.com>
2024-10-24 10:01:21 -0400

    react drop zone explaination

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import "./App.css";
 import { ToastContainer } from "react-toastify";
 import FileDropper from "./FileDroper";
 
+/**
+ * The ReactDropZone function renders a file dropper component with specified props and a toast
+ * container for notifications.
+ * @returns The `ReactDropZone` function is returning a JSX fragment containing a `ToastContainer`
+ * component and a `FileDropper` component. The `ToastContainer` component is used for displaying toast
+ * notifications at the bottom-right position with an auto-close duration of 3000 milliseconds. The
+ * `FileDropper` component is passed the props `id`, `uploadFile`, `types`, `
+ */
 function ReactDropZone({ id, uploadFile, types, allowMultiple, style }) {
   return (
     <>
